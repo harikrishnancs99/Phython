66c2938a68f76f978faf69b9de46d5b20ad903b8
harikrishnancs99 <harikrishnancs13@gmail.com>
2021-11-25 15:47:26 +0530

    first commit

---
+++ greaterorsmaller.py
@@ -0,0 +1,7 @@
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+if x>y:
+    print(f"{x} is greater number\n")
+else:
+    print(f"{y} is greater number\n")
+    
---
+++ smallamongthreenumbers.py
@@ -0,0 +1,11 @@
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+z=int(input("Enter the third number:"))
+if x<y:
+    print(f"{x} is smaller number\n")
+elif y<z:
+    print(f"{y} is smaller number\n")
+else:
+    print(f"{z} is smaller number\n")
+        
+    
